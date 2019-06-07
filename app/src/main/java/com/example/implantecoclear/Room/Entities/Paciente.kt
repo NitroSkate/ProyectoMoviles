ac891eddd281ea960081cce56ccac2c89103b25a
Alex7498-SV <00068817@uca.edu.sv>
2019-06-06 21:38:31 -0600

    n/a

--- app/src/main/java/com/example/implantecoclear/Room/Entities/Paciente.kt
+++ app/src/main/java/com/example/implantecoclear/Room/Entities/Paciente.kt
@@ -1,5 +1,6 @@
 package com.example.implantecoclear.Room.Entities
 
+import android.os.Parcel
 import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
@@ -9,11 +10,50 @@ import androidx.room.PrimaryKey
 class Paciente(
     @ColumnInfo(name = "P_nombre")
     var nombre : String,
+    @ColumnInfo(name = "P_apellido")
+    var apellido : String,
+    @ColumnInfo(name = "P_ingreso")
+    var fechaIngreso : String,
     @ColumnInfo(name = "P_nivel")
-    var nivel : Int
+    var nivel : Int,
+    @ColumnInfo(name = "P_idTerapista")
+    var id_terapiste : Int
 
     ): Parcelable {
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "P_id")
     var id : Int = 0
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readInt(),
+        parcel.readInt()
+    ) {
+        id = parcel.readInt()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(nombre)
+        parcel.writeString(apellido)
+        parcel.writeString(fechaIngreso)
+        parcel.writeInt(nivel)
+        parcel.writeInt(id_terapiste)
+        parcel.writeInt(id)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Paciente> {
+        override fun createFromParcel(parcel: Parcel): Paciente {
+            return Paciente(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Paciente?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
---
+++ app/src/main/java/com/example/implantecoclear/Room/Entities/Terapista.kt
@@ -0,0 +1,10 @@
+package com.example.implantecoclear.Room.Entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+
+@Entity(tableName = "Terapiste")
+class Terapista(
+    @ColumnInfo(name = "")
+) {
+}
