c0ee768af99cb518fb5f398c1284222176e4a663
Alex7498-SV <00068817@uca.edu.sv>
2019-06-22 09:02:36 -0600

    Correccion de la pantalla de login

--- app/src/main/java/com/polillas/cocleapp/fragmentos/AccountFragment.kt
+++ app/src/main/java/com/polillas/cocleapp/fragmentos/AccountFragment.kt
@@ -3,6 +3,7 @@ package com.polillas.cocleapp.fragmentos
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,7 +16,6 @@ import android.widget.Toast
 import com.polillas.cocleapp.R
 import kotlinx.android.synthetic.main.fragment_account.view.*
 import kotlinx.android.synthetic.main.login.*
-import kotlinx.android.synthetic.main.login.send_bt
 import kotlinx.android.synthetic.main.login.view.*
 
 // TODO: Rename parameter arguments, choose names that match
@@ -39,13 +39,13 @@ class AccountFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_account, container, false)
         view.login.setOnClickListener {
             var popup = inflater.inflate(R.layout.login, null).apply {
-                send_bt.text = "Iniciar Sesion"
+                Log.d("cuenta", "Fragmento lanzado")
             }
             var popupview = PopupWindow(popup, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, true)
 
             popupview.showAtLocation(it, Gravity.CENTER,0,0)
             popup.apply {
-                send_bt.setOnClickListener {
+                lL_login.setOnClickListener {
                     popupview.dismiss()
                     Toast.makeText(it.context, "Cuenta creada", Toast.LENGTH_SHORT).show()
                 }
