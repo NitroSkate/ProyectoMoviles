2921d615dd326de68be189f7018a79793bcccc36
Alex7498-SV <00068817@uca.edu.sv>
2019-07-01 22:06:22 -0600

    Finalizacion de comentarios en room

--- app/src/main/java/com/polillas/cocleapp/Room/RoomDB/CoclearDatabase.kt
+++ app/src/main/java/com/polillas/cocleapp/Room/RoomDB/CoclearDatabase.kt
@@ -7,15 +7,16 @@ import androidx.room.RoomDatabase
 import com.polillas.cocleapp.Room.DAO.*
 import com.polillas.cocleapp.Room.Entities.*
 
-@Database(entities = arrayOf(Pregunta::class/*, Paciente::class, Terapeuta::class, Sonido::class*/, Sonido::class), version = 1, exportSchema = false)
-public abstract class CoclearDatabase : RoomDatabase(){
+/*TODO esta clase se encarga de hacer la instancia de la base de datos*/
 
-    //abstract fun pruebaDAO() : PruebaDao
+@Database(entities = arrayOf(Pregunta::class, Sonido::class), version = 1, exportSchema = false)
+abstract class CoclearDatabase : RoomDatabase(){
+
+    //TODO se crean instancias de los DAO a utilizar
     abstract fun sonidoDAO() : SonidoDAO
-    //abstract fun pacienteDAO() : PacienteDAO
     abstract fun preguntaDAO() : PreguntaDAO
-    //abstract fun terapistaDAO() : TerapistaDAO
 
+    //TODO aqui se verifica si la base de datos ya esta creada, de ser asi solo se retorna esa instancia de lo contrario se crea una nueva
     companion object{
         @Volatile
         private var INSTANCE : CoclearDatabase? = null
--- app/src/main/java/com/polillas/cocleapp/Room/Viewmodel/PreguntaViewmodel.kt
+++ app/src/main/java/com/polillas/cocleapp/Room/Viewmodel/PreguntaViewmodel.kt
@@ -13,16 +13,22 @@ import com.polillas.cocleapp.Room.RoomDB.CoclearDatabase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+//TODO en esta clase se crea el viewmodel de las preguntas
+
 class PreguntaViewmodel(application: Application):AndroidViewModel(application){
 
+    //TODO instancia de repositorio de preguntas
     private var repoPre: PreguntaRepo?=null
 
+    //TODO se inicializan variables con los DAO de la base de datos room
     init{
         val preguntaDao = CoclearDatabase.getDatabase(getApplication()).preguntaDAO()
         val sonidoDao = CoclearDatabase.getDatabase(getApplication()).sonidoDAO()
         repoPre = PreguntaRepo(preguntaDao, sonidoDao)
     }
 
+    //TODO en esta funcion se trata de obtener datos de la API en caso esto falle se enviara un mensaje de error
+    //Caso contrario se limpiara la base y se almacenaran los nuevos datos
     fun retrievePreguntas() = viewModelScope.launch {
         this@PreguntaViewmodel.nukeQ()
         this@PreguntaViewmodel.nukeS()
@@ -42,24 +48,27 @@ class PreguntaViewmodel(application: Application):AndroidViewModel(application){
         } else with(response){
             when(this.code()){
                 404-> {
-                    Toast.makeText(getApplication(), "Se murio", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(getApplication(), "ERROR: archivo no encontrado", Toast.LENGTH_SHORT).show()
                 }
             }
         }
     }
 
+    //TODO esta funcion inserta las preguntas dentro de su respectiva tabla
     fun insertQuestion(pregunta: Pregunta?) = viewModelScope.launch (Dispatchers.IO){
         repoPre!!.insertPregunta(pregunta)
     }
-
+    //TODO esta funcion inserta los recursos de la pregunta correspondiente dentro de su respectiva tabla
     fun insertSound(sonido: Sonido?) = viewModelScope.launch (Dispatchers.IO){
         repoPre!!.insertSonido(sonido)
     }
 
+    //TODO las funciones siguientes se encargan de obtener los datos de la base de datos para devolverlos al momento de ser llamada
     fun getAllPreguntas(): LiveData<List<Pregunta>> = repoPre!!.getAllPreguntas()
 
     fun getAllSonidos(): LiveData<List<Sonido>> = repoPre!!.getSonidos()
 
+    //TODO las funciones siguientes se encargan de limpiar la base de datos
     fun nukeQ() = viewModelScope.launch (Dispatchers.IO){
         repoPre!!.nukeQ()
     }
